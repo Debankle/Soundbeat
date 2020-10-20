60236c0e1fc565ff46b9a0cba59efeeacdb9d2ad
Debankle <oliver.debankle@gmail.com>
2020-10-20 17:42:00 +1000

    removed test text

--- src/react.tsx
+++ src/react.tsx
@@ -8,7 +8,6 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Poopy brain</h1>
                 <Header />
                 <Playbar />
                 <Sidebar />
